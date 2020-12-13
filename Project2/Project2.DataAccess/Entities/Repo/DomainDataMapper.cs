fd8442b6c2334d49fd33cc9b2c4cb8419c8ec2a8
Ryan Towner <townerr@yahoo.com>
2020-12-12 18:00:23 -0600

    Change AppCard Id to CardId Added public to constructors added preferences to domain project

--- Project2/Project2.DataAccess/Entities/Repo/DomainDataMapper.cs
+++ Project2/Project2.DataAccess/Entities/Repo/DomainDataMapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project2.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Project2/Project2.Domain/AppCard.cs
+++ Project2/Project2.Domain/AppCard.cs
@@ -6,17 +6,22 @@ namespace Project2.Domain
 {
     public class AppCard
     {
-        public string Id { get; set; }
+        public string CardId { get; set; }
         public AppPack Pack { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public int Rarity { get; set; }
         public double Value { get; set; }
 
-        AppCard(string id, AppPack pack)
+        public AppCard()
         {
-            this.Id = id;
-            this.Pack = pack;
+     
+        }
+
+        public AppCard(string id, AppPack pack)
+        {
+            CardId = id;
+            Pack = pack;
         }
         
     }
