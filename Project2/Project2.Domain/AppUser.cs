38395f1ecc5fd54b2ea4235b4976fe23528aed8b
Joseph Ellis <josephtomellis@gmail.com>
2020-12-12 21:02:05 -0600

    modified appuser names

--- Project2/Project2.Domain/AppUser.cs
+++ Project2/Project2.Domain/AppUser.cs
@@ -6,24 +6,26 @@ namespace Project2.Domain
 {
     public class AppUser
     {
-        public string Id { get; }
-        public string FirstName { get; }
-        public string LastName { get; }
+        public string UserId { get; }
+        public string First { get; }
+        public string Last { get; }
         public string Email { get;}
         public bool IsManager { get; }
+        public string UserRole { get; }
+        public int NumPacksPurchased { get; set; } = 0;
         public double CurrencyAmount { get; set; }
         public List<AppCard> Inventory { get; set; } = new List<AppCard>();
         public List<AppAuction> AuctionHistory { get; set; } = new List<AppAuction>();
         public List<AppOrder> OrderHistory { get; set; } = new List<AppOrder>();
         public List<AppTrade> TradeHistory { get; set; } = new List<AppTrade>();
 
-        AppUser(string id, string firstName, string lastName, string email, bool isManager)
+        AppUser(string userId, string firstName, string lastName, string email, string UserRole)
         {
-            this.Id = id;
-            this.FirstName = firstName;
-            this.LastName = lastName;
+            this.UserId = userId;
+            this.First = firstName;
+            this.Last = lastName;
             this.Email = email;
-            this.IsManager = isManager;
+            this.UserRole = UserRole;
         }
     }
 }
