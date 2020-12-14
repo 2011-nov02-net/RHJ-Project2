0db496a1442c1f6f575d2bbb18a35f3d90ba9ec8
Hao <haoyang439@gmail.com>
2020-12-14 10:41:46 -0700

    PackRepo and OrderRepo

---
+++ Project2/Project2.DataAccess/Entities/Repo/IOrderRepo.cs
@@ -0,0 +1,15 @@
+﻿using Project2.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.DataAccess.Entities.Repo
+{
+    public interface IOrderRepo
+    {
+        Task<IEnumerable<AppOrder>> GetAllOrders();
+        Task<AppOrder> GetOneOrder(string id);
+        Task<Order> GetOneOrderDetail(string id);
+    }
+}
--- Project2/Project2.DataAccess/Entities/Repo/PackRepo.cs
+++ Project2/Project2.DataAccess/Entities/Repo/PackRepo.cs
@@ -1,21 +1,39 @@
-﻿using Project2.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using Project2.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Project2.DataAccess.Entities.Repo
 {
     public class PackRepo : IPackRepo
     {
-        public Task<AppPack> GetAllPacks()
+
+        private readonly DbContextOptions<Project2Context> _contextOptions;
+        public PackRepo(DbContextOptions<Project2Context> contextOptions)
         {
-            throw new NotImplementedException();
+            _contextOptions = contextOptions;
         }
 
-        public Task<AppPack> GetOnePack(string id)
+        // not mapped
+        public async Task<AppPack> GetAllPacks()
         {
-            throw new NotImplementedException();
+            using var context = new Project2Context(_contextOptions);
+            var dbPacks = await context.Packs.ToListAsync();
+            if (dbPacks == null) return null;
+            var appPacks = dbPacks.Select(x => new AppPack(x.PackId, x.Name, x.Price, x.DateReleased));
+            return appPacks;    
+        }
+
+        public async Task<AppPack> GetOnePack(string id)
+        {
+            using var context = new Project2Context(_contextOptions);
+            var dbPack = await context.Packs.FirstOrDefaultAsync(x => x.PackId == id);
+            if (dbPack == null) return null;
+            var appPack = new AppPack(dbPack.PackId, dbPack.Name, dbPack.Price, dbPack.DateReleased);
+            return appPack;
         }
     }
 }
