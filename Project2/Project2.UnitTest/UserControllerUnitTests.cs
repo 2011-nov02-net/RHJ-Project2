d5bc0ac6020bf0a36aeaae40a2fc594a325cf9f5
Hao <haoyang439@gmail.com>
2020-12-15 18:57:32 -0700

    Version aligned and Moq ready

---
+++ Project2/Project2.UnitTest/UserControllerUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using Project2.Api.DTO;
+using Project2.DataAccess.Entities.Repo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2.UnitTest
+{
+    public class UserControllerUnitTests
+    {
+        /*
+        public IEnumerable<UserReadDTO> UserController_GetAllUsers()
+        {
+            var _mockRepo = new Mock<IUserRepo>;
+
+            
+        }
+        */
+
+    }
+}
