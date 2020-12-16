5d2ada609562a9b6bb799e27739b7fbce133e2ce
Ryan Towner <townerr@yahoo.com>
2020-12-15 18:56:32 -0600

    Completed AuctionRepo Auctions to DataMapper

---
+++ Project2/Project2.DataAccess/Entities/Repo/IAuctionRepo.cs
@@ -0,0 +1,18 @@
+﻿using Project2.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.DataAccess.Entities.Repo
+{
+    public interface IAuctionRepo
+    {
+        Task<IEnumerable<AppAuction>> GetAllAuctions();
+        Task<AppAuction> GetAuctionById(string id);
+        Task<AppAuction> GetAuctionDetailById(string id);
+        Task<bool> CreateAuction(AppAuction auction);
+        Task<bool> UpdateAuction(string id, AppAuction auction);
+        Task<bool> UpdateAuctionDetail(string id, AppAuction auction);
+    }
+}
