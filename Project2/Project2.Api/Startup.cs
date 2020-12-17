c170de82316edec1362a4d67147ce646cd6c5639
Ryan Towner <townerr@yahoo.com>
2020-12-17 14:08:07 -0600

    Add interfaces reference in startup for repos

--- Project2/Project2.Api/Startup.cs
+++ Project2/Project2.Api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Project2.DataAccess.Entities.Repo;
+using Project2.DataAccess.Entities.Repo.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
