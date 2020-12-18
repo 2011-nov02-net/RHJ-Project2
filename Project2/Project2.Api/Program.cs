96f20332d68f009c80bca48ef435eb5669ec1907
Ryan Towner <townerr@yahoo.com>
2020-12-18 11:35:55 -0600

    Added method to create cards from pokemontcg api

--- Project2/Project2.Api/Program.cs
+++ Project2/Project2.Api/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Project2.DataAccess.Entities;
+using Project2.DataAccess.Entities.Repo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
