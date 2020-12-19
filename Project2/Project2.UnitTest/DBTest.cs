1a2ad2945fa22d4540d19cf5d346d69be3019ee7
Hao <haoyang439@gmail.com>
2020-12-19 14:03:14 -0700

    Mapper passed unit tests

---
+++ Project2/Project2.UnitTest/DBTest.cs
@@ -0,0 +1,85 @@
+﻿using Project2.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.UnitTest
+{
+    public class DBTest
+    {
+        public static List<Customer> DBUsers()
+        {
+            var sessions = new List<Customer>();
+            sessions.Add(new Customer
+            {
+                UserId = "cus1",
+                First = "Kyle",
+                Last = "Crane",
+                Email = "KC@gmail.com",
+                UserRole = "User",
+                NumPacksPurchased = 1,
+                CurrencyAmount = 10,
+            });
+            sessions.Add(new Customer
+            {
+                UserId = "cus2",
+                First = "NaN",
+                Last = "Man",
+                Email = "NN@gmail.com",
+                UserRole = "User",
+                NumPacksPurchased = 2,
+                CurrencyAmount = 20,
+            });
+            return sessions;
+        }
+
+        public static List<Card> DBCards()
+        {
+            var sessions = new List<Card>();
+            sessions.Add(new Card
+            {
+                CardId = "card101",
+                Name = "Mew",
+                Type = "Psychic",
+                Rarity = 5,
+                Value = 10,
+            });
+            sessions.Add(new Card
+            {
+                CardId = "card102",
+                Name = "MewTwo",
+                Type = "Psychic",
+                Rarity = 6,
+                Value = 15,
+            });
+            sessions.Add(new Card
+            {
+                CardId = "card103",
+                Name = "Pinsir",
+                Type = "Bug",
+                Rarity = 4,
+                Value = 5,
+            });
+            return sessions;
+        }
+
+        public static List<StoreInventory> DBItems()
+        {
+            var sessions = new List<StoreInventory>();
+            sessions.Add(new StoreInventory
+            {
+                PackId = "pack101",
+                PackQty = 10,
+            });
+            sessions.Add(new StoreInventory
+            {
+                PackId = "pack101",
+                PackQty = 10,
+            });
+            return sessions;
+        }
+
+    }
+}
--- Project2/Project2.UnitTest/MapperUnitTests.cs
+++ Project2/Project2.UnitTest/MapperUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using Project2.Api.DTO;
+using Project2.DataAccess.Entities.Repo;
 using Project2.Domain;
 using System;
 using System.Collections.Generic;
@@ -12,13 +13,61 @@ namespace Project2.UnitTest
 {
     public class MapperUnitTests
     {
-        [Fact]            
-        public void User_Mapping_Test()
+        [Fact]
+        public void DomainDateMapper_User_GetOneUser()
         {
-             
-
-
+            var dbUser = DBTest.DBUsers()[0];
+            var appUser = DomainDataMapper.GetOneUser(dbUser);
+            Assert.Equal(dbUser.UserId, appUser.UserId);
+            Assert.Equal(dbUser.First, appUser.First);
+            Assert.Equal(dbUser.Last, appUser.Last);
+            Assert.Equal(dbUser.Email, appUser.Email);
+            Assert.Equal(dbUser.UserRole, appUser.UserRole);
+            Assert.Equal(dbUser.NumPacksPurchased, appUser.NumPacksPurchased);
+            Assert.Equal(dbUser.CurrencyAmount, appUser.CurrencyAmount);
         }
 
+        [Fact]            
+        public void DomainDateMapper_User_AddOneUser()
+        {
+            var appUser = Test.Users()[0];
+            var dbUser = DomainDataMapper.AddOneUser(appUser);
+            Assert.Equal(expected: appUser.UserId, actual: dbUser.UserId);
+            Assert.Equal(appUser.First, dbUser.First);
+            Assert.Equal(appUser.Last, dbUser.Last);
+            Assert.Equal(appUser.Email, dbUser.Email);
+            Assert.Equal(appUser.UserRole, dbUser.UserRole);
+            Assert.Equal(appUser.NumPacksPurchased, dbUser.NumPacksPurchased);
+            Assert.Equal(appUser.CurrencyAmount, dbUser.CurrencyAmount);
+        }
+
+        [Fact]
+        public void DomainDateMapper_Card_GetOneCard()
+        {
+            var dbCard = DBTest.DBCards()[0];
+            var appCard = DomainDataMapper.GetOneCard(dbCard);
+            Assert.Equal(dbCard.CardId, appCard.CardId);
+            Assert.Equal(dbCard.Name, appCard.Name);
+            Assert.Equal(dbCard.Type, appCard.Type);
+        }
+
+        [Fact]
+        public void DomainDateMapper_Card_AddOneCard()
+        {
+            var appCard = Test.Cards()[0];
+            var dbCard = DomainDataMapper.AddOneCard(appCard);
+            Assert.Equal(appCard.CardId, dbCard.CardId);
+            Assert.Equal(appCard.Name, dbCard.Name);
+            Assert.Equal(appCard.Type, dbCard.Type);
+        }
+
+        [Fact]
+        public void DomainDateMapper_Store_GetOneItem()
+        {
+            var dbItem = DBTest.DBItems()[0];
+            var appItem = DomainDataMapper.GetStorePackById(dbItem);
+            Assert.Equal(dbItem.PackId, appItem.PackId);
+            Assert.Equal(dbItem.PackQty, appItem.PackQty);
+        }
     }
 }
