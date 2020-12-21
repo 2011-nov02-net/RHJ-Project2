3cacf373a824ffaa1f12b68f7a8f2bf6a88a9bff
Hao <haoyang439@gmail.com>
2020-12-20 22:45:53 -0700

    Added about, nav components, used environments

--- Project2-Angular/pokemon-app/src/app/services/backend.service.ts
+++ Project2-Angular/pokemon-app/src/app/services/backend.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
- 
+
+// import dev environment
+import { environment} from '../../environments/environment';
+// need to be added
+import { environmentProd} from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
   // to be replaced by app service url
-  private baseUrl = 'https://localhost:44301/api'
+  private baseUrl =  'https://localhost:44301/api';
+  // private baseUrl = ` ${environment.baseUrl}`;
 
   constructor(private http:HttpClient) { }
   
