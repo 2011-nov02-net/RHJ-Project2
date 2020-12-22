17dfa6c93683374ae45d03e6cf81d4d5867ebd5a
Joseph Ellis <josephtomellis@gmail.com>
2020-12-22 12:57:25 -0600

    getOrder

--- Project2-Angular/pokemon-app/src/app/components/store/store.component.ts
+++ Project2-Angular/pokemon-app/src/app/components/store/store.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { BackendService } from '../../services/backend.service';
-//import {Pack} from '../../interfaces/pack';
+import {Pack} from '../../interfaces/pack';
+import {Order} from '../../interfaces/order';
 
 @Component({
   selector: 'app-store',
@@ -10,12 +11,19 @@ import { BackendService } from '../../services/backend.service';
 })
 export class StoreComponent implements OnInit {
 
-    packs: any;
+    packs: Pack[] | any;
+    orders: Order[] | any;
 
   constructor(private backEndService: BackendService  ) { }
 
   ngOnInit(): void {
-    //this.backEndService.getStoreItems().subscribe((data) => { this.packs  = data; })
+    this.getStorePacks();
   }
 
+  getStorePacks(): void{
+    this.backEndService.getStorePacks().subscribe(packs => { this.packs  = packs; })
+  }
+  sendOrder(input: number): void{
+
+  }
 }
--- Project2-Angular/pokemon-app/src/app/services/backend.service.ts
+++ Project2-Angular/pokemon-app/src/app/services/backend.service.ts
@@ -11,6 +11,8 @@ import { User } from '../interfaces//user';
 import { Card } from '../interfaces/card';
 import { Trade } from '../interfaces/trade'
 import { Auction } from '../interfaces/auction'
+import { Pack } from '../interfaces/pack'
+import { Order } from '../interfaces/order'
 
 @Injectable({
   providedIn: 'root'
@@ -52,11 +54,12 @@ export class BackendService {
     return this.http.get<Auction[]>(this.baseUrl + '/auctions');
   }
 
-  getStorePacks()
+  getStorePacks(): Observable<Pack[]>
   {
-    // userId hard coded, replce with login
-    return this.http.get(`${this.baseUrl}/store`);
+    return this.http.get<Pack[]>(`${this.baseUrl}/store`);
   }
-
+  postOrder(order: Order): Observable<Order> {
+    return this.http.post<Order>(this.baseUrl + '/order', order);
+  }
 
 }
