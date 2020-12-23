7002baca6a332163054a133cb35efa5b1e64faf2
Ryan Towner <townerr@yahoo.com>
2020-12-23 16:36:49 -0600

    changed to prod

--- Project2-Angular/pokemon-app/src/app/services/backend.service.ts
+++ Project2-Angular/pokemon-app/src/app/services/backend.service.ts
@@ -21,7 +21,7 @@ import { OrderItem } from '../interfaces/orderItem'
 export class BackendService {
   // to be replaced by app service url
   //private baseUrl =  'https://localhost:44301/api';
-  private baseUrl = `${environment.baseUrl}`;
+  private baseUrl = `${environmentProd.baseUrl}`;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
