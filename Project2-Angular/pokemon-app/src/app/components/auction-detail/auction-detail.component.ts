5e030839d908a8ff0d1b0a2618cca790b05b0d2d
Ryan Towner <townerr@yahoo.com>
2020-12-23 23:04:01 -0600

    auction bid/buyout functions

--- Project2-Angular/pokemon-app/src/app/components/auction-detail/auction-detail.component.ts
+++ Project2-Angular/pokemon-app/src/app/components/auction-detail/auction-detail.component.ts
@@ -17,6 +17,7 @@ export class AuctionDetailComponent implements OnInit {
   email: any;
   buyer: User | any;
   offerer: string | any;
+  amount: number = 0;
 
   constructor(private backend: BackendService, private route: ActivatedRoute) { }
 
@@ -40,6 +41,73 @@ export class AuctionDetailComponent implements OnInit {
   getCard(): void {
     let cid = this.route.snapshot.params.cid;
     this.backend.getCardById(cid).subscribe(card => this.card = card);
+  }
+
+  bidChange(num:any): void {
+    this.amount = num.target.value;
+  }
+
+  placeBid(): void {
+    let user: User = {
+      userId: this.buyer.userId,
+      first: this.buyer.first,
+      last: this.buyer.last,
+      email: this.buyer.email,
+      numPacksPurchased: this.buyer.numPacksPurchased,
+      currencyAmount: this.buyer.currencyAmount - this.amount,
+      userRole: "1"
+    }
+
+    let increase:number = this.amount - this.auction.priceListed;
+
+    let auction: Auction = {
+      auctionId: this.auction.auctionId,
+      sellerId: this.auction.sellerId,
+      buyerId: this.buyer.userId,
+      cardId: this.auction.cardId,
+      priceListed: this.auction.priceListed + increase,
+      buyoutPrice: this.auction.buyoutPrice,
+      sellType: "Bid",
+      priceSold: this.auction.priceSold,
+      numberBids: this.auction.numberBids,
+      sellDate: this.auction.sellDate,
+      expDate: this.auction.expDate
+    }
+    //update user currency and auction bid amount
+    this.backend.updateUserById(this.buyer.userId, user).subscribe();
+    this.backend.updateAuctionById(this.auction.auctionId, auction).subscribe();
+    alert("Placed Bid Successfully!");
+
+  }
+
+  buyNow(): void {
+    let user: User = {
+      userId: this.buyer.userId,
+      first: this.buyer.first,
+      last: this.buyer.last,
+      email: this.buyer.email,
+      numPacksPurchased: this.buyer.numPacksPurchased,
+      currencyAmount: this.buyer.currencyAmount - this.auction.buyoutPrice,
+      userRole: "1"
+    }
+
+    let auction: Auction = {
+      auctionId: this.auction.auctionId,
+      sellerId: this.auction.sellerId,
+      buyerId: this.buyer.userId,
+      cardId: this.auction.cardId,
+      priceListed: this.auction.buyoutPrice,
+      buyoutPrice: this.auction.buyoutPrice,
+      sellType: this.auction.sellType,
+      priceSold: this.auction.buyoutPrice,
+      numberBids: this.auction.numberBids,
+      sellDate: new Date(),
+      expDate: this.auction.expDate
+    }
+    //update user currency and auction bid amount
+    this.backend.updateUserById(this.buyer.userId, user).subscribe();
+    this.backend.updateAuctionById(this.auction.auctionId, auction).subscribe();
+    alert("Bought Out Auction Successfully!");
   }
 
 }
