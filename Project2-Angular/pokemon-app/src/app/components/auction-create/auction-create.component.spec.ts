a3afcfc7895d523eda26e17301189c6b82c03dad
Ryan Towner <townerr@yahoo.com>
2020-12-23 19:23:20 -0600

    auctions stuff

---
+++ Project2-Angular/pokemon-app/src/app/components/auction-create/auction-create.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuctionCreateComponent } from './auction-create.component';
+
+describe('AuctionCreateComponent', () => {
+  let component: AuctionCreateComponent;
+  let fixture: ComponentFixture<AuctionCreateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AuctionCreateComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuctionCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
