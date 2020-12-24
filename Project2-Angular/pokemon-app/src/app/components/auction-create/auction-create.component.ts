5225e9129fc644c81ba161a56ead7050dc598a03
Ryan Towner <townerr@yahoo.com>
2020-12-23 20:38:35 -0600

    added create auction

--- Project2-Angular/pokemon-app/src/app/components/auction-create/auction-create.component.ts
+++ Project2-Angular/pokemon-app/src/app/components/auction-create/auction-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Auction } from 'src/app/interfaces/auction';
+import { Card } from 'src/app/interfaces/card';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-auction-create',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuctionCreateComponent implements OnInit {
 
-  constructor() { }
+  userCards: Card[] | any;
+  card: Card | any;
+  userId: any;
+
+  constructor(private backend: BackendService) { }
 
   ngOnInit(): void {
+    this.userId = JSON.parse(localStorage.getItem('id') || '{}');
+    this.getUserInventory(this.userId.id);
+  }
+
+  //get users specific inventory
+  getUserInventory(userId:string): void {
+    this.backend.getUserCardsById(userId).subscribe(cards => this.userCards = cards);
+  }
+
+  onSelectedCard(cardId:string): void{
+    this.backend.getCardById(cardId).subscribe(card => this.card = card);
   }
 
 }
